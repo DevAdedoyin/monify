b99ef2d60072c1bc1334daaad0a670923e838126
Adedoyin <isradeoluwa@gmail.com>
2023-09-18 16:02:27 +0100

    icons and icon component created

---
+++ src/components/Icons.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+// import { IconContext } from "react-icons";
+
+export default function Icons(icon: string) {
+  return (
+    <div>
+      <img src={icon} />
+    </div>
+  );
+}
