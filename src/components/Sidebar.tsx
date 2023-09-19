97e2e7b445ebb36b3b7e667de28a3d68fb5b71c1
Adedoyin <isradeoluwa@gmail.com>
2023-09-19 16:50:24 +0100

    Navigation created

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { Component, ReactNode } from "react";
 
-export default function Sidebar() {
+interface Props {
+  children?: ReactNode;
+  // any props that come into the component
+}
+
+export default function Sidebar({ children }: Props) {
   return (
-    <div>Sidebar</div>
-  )
+    <div>
+      <main>{children}</main>
+    </div>
+  );
 }
