71f061eabe92b9b76153da2c4a4f51e749a91849
Adedoyin <isradeoluwa@gmail.com>
2023-09-23 15:25:37 +0100

    Username done

--- src/components/Dashboard/DashboardHeader.tsx
+++ src/components/Dashboard/DashboardHeader.tsx
@@ -2,17 +2,15 @@ import React from "react";
 import DHStyle from "./DashboardHeader.module.css";
 import Input from "@material-ui/core/Input";
 import InputAdornment from "@material-ui/core/InputAdornment";
-// import SearchIcon from "@material-ui/icons-material/Search";
 import { FaSearch } from "react-icons/fa";
-
-// type Props = {}
+import Badge from "react-badger";
 
 function DashboardHeader() {
   return (
     <div className={DHStyle.headerContainer}>
       <div className={DHStyle.inputContainer}>
         <Input
-          placeholder="Search Anything"
+          placeholder="Search Anything..."
           // variant="outlined"
           className={DHStyle.input}
           endAdornment={
@@ -20,11 +18,22 @@ function DashboardHeader() {
               <FaSearch className={DHStyle.searchIcon} />
             </InputAdornment>
           }
-          style={{color: "white", fontSize: "0.7rem"}}
+          style={{ color: "white", fontSize: "0.7rem" }}
           disableUnderline={true}
         />
       </div>
-      <div className={DHStyle.headerDetailsContainer}></div>
+      <div className={DHStyle.headerDetailsContainer}>
+        <div className={DHStyle.notificationStyle}>{/* <Badge /> */}</div>
+        <div>{/* <p>EN</p> */}</div>
+        <div className={DHStyle.vr}></div>
+        <div className={DHStyle.usernameContainer}>
+          <p className={DHStyle.username}>Adedoyin Idris</p>
+          <p className={DHStyle.rank}>Super Admin</p>
+        </div>
+        <div className={DHStyle.profilePicContainer}>
+          <p className={DHStyle.userInitials}>AI</p>
+        </div>
+      </div>
     </div>
   );
 }
