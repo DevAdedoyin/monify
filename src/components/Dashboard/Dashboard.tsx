5226fde6ca3c905285fe49690490235154ce427f
Adedoyin <isradeoluwa@gmail.com>
2023-09-30 08:10:26 +0100

    Tabs styling completed

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import DashboardHeader from "./DashboardHeader";
+import TabsComponent from "../TabsComponent";
 
 function Dashboard() {
   return (
     <div>
       <DashboardHeader />
+      <TabsComponent />
     </div>
   );
 }
